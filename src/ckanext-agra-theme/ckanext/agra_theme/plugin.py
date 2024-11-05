b9b50fa6dc4adfc874f581d777e9ccc25945a2cb
dedenbangkit <mail@dedenbangkit.com>
2024-11-05 20:01:00 +0700

    [#183] Page behind login example

--- src/ckanext-agra-theme/ckanext/agra_theme/plugin.py
+++ src/ckanext-agra-theme/ckanext/agra_theme/plugin.py
@@ -8,7 +8,6 @@ class AgraThemePlugin(plugins.SingletonPlugin):
     # IConfigurer
 
     def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic',
-            'agra_theme')
+        toolkit.add_template_directory(config_, "templates")
+        toolkit.add_public_directory(config_, "public")
+        toolkit.add_resource("fanstatic", "agra_theme")
