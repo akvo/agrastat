30e05d89cc9316f3838eaa2b97fcceb1267ba727
dedenbangkit <mail@dedenbangkit.com>
2024-11-07 12:49:45 +0700

    [#1] Create middleware for page behind login

--- src/ckanext-agra-theme/ckanext/agra_theme/plugin.py
+++ src/ckanext-agra-theme/ckanext/agra_theme/plugin.py
@@ -1,13 +1,53 @@
+from os import environ
+from werkzeug.wrappers import Response
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
 
 class AgraThemePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IMiddleware, inherit=True)
+
+    def make_middleware(self, app, config):
+        # Wrap the CKAN app with our custom middleware
+        return AgraThemeMiddleware(app)
 
     # IConfigurer
-
     def update_config(self, config_):
+        # Add the template, public, and resource directories for the theme
         toolkit.add_template_directory(config_, "templates")
         toolkit.add_public_directory(config_, "public")
         toolkit.add_resource("fanstatic", "agra_theme")
+
+
+class AgraThemeMiddleware:
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        # Check if the user is logged in based on the presence of 'REMOTE_USER'
+        user = environ.get("REMOTE_USER")
+
+        # Define allowed paths for non-logged-in users
+        path = environ.get("PATH_INFO", "")
+        allowed_paths = [
+            "/",  # Home page
+            "/user/login",  # Login page
+            "/user/reset",  # Reset password page
+        ]
+        resource_paths = ["/base/", "/public/", "/fanstatic/"]
+        allowed_resource = [".jpg", ".png", ".css", ".js"]
+        if path.startswith(tuple(resource_paths)) or path.endswith(
+            tuple(allowed_resource)
+        ):
+            return self.app(environ, start_response)
+
+        # Redirect non-logged-in users trying to access any other page
+        if not user and path not in allowed_paths:
+            # Create a redirect response to the login page
+            response = Response("Redirecting to login...", status=302)
+            response.headers["Location"] = "/user/login"
+            return response(environ, start_response)
+
+        # Continue with the original request if logged in or on an allowed path
+        return self.app(environ, start_response)
