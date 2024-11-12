acdbca241dc3d14242d9ea408699a481409b39ae
dedenbangkit <mail@dedenbangkit.com>
2024-11-12 15:08:56 +0700

    [#4] Custom sidebar and statistic

--- src/ckanext-agra-theme/ckanext/agra_theme/plugin.py
+++ src/ckanext-agra-theme/ckanext/agra_theme/plugin.py
@@ -1,13 +1,58 @@
 from os import environ
 from werkzeug.wrappers import Response, Request
+from flask import Blueprint, render_template, abort
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 import logging
+
+file_size_blueprint = Blueprint("file_size", __name__)
+
+
+@file_size_blueprint.route("/dataset/<dataset_id>/file_size")
+def file_size(dataset_id):
+    # Set up the context for accessing CKAN actions
+    context = {"user": toolkit.g.user}
+
+    try:
+        # Retrieve the dataset and its resources using CKAN actions
+        dataset = toolkit.get_action("package_show")(
+            context, {"id": dataset_id}
+        )
+        resources = dataset["resources"]
+    except toolkit.ObjectNotFound:
+        abort(404, description="Dataset not found")
+
+    # Prepare a list to store resource file sizes
+    resource_sizes = []
+    for resource in resources:
+        size = resource.get("size")
+
+        # If the size is None, try to retrieve it from the URL
+        if not size:
+            url = resource.get("url")
+            try:
+                response = requests.head(url, allow_redirects=True)
+                size = response.headers.get("Content-Length")
+            except requests.RequestException:
+                size = None
+
+        # Append to list with resource name and size
+        resource_sizes.append(
+            {"name": resource.get("name"), "size": size if size else "Unknown"}
+        )
+
+    # Render a template with the resource size data
+    return toolkit.render("file_size.html", {"resource_sizes": resource_sizes})
 
 
 class AgraThemePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IMiddleware, inherit=True)
+    plugins.implements(plugins.IBlueprint)
+    plugins.implements(plugins.IRoutes, inherit=True)
+
+    def get_blueprint(self):
+        return file_size_blueprint
 
     def make_middleware(self, app, config):
         # Wrap the CKAN app with our custom middleware
