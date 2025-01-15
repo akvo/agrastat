ca892d8edd359455e0370d069f15d49193993a70
dedenbangkit <mail@dedenbangkit.com>
2025-01-15 14:43:26 +0700

    [#5][#9] Agrovoc child, broader suggestions, style improvements

--- src/ckanext-agra-theme/ckanext/agra_theme/public/base/javascript/filters.js
+++ src/ckanext-agra-theme/ckanext/agra_theme/public/base/javascript/filters.js
@@ -1,39 +1,71 @@
-// country list
-// only for /dataset/edit/xxx or /dataset/new
+/* Agrovoc tags */
+function handleTagChange(tags, query_params = "") {
+  var tag = tags[tags.length - 1];
+  var tag_suggestions = $("#tag-suggestions");
+  var api_url = "/api/2/util/tag/suggestion?q=" + tag + query_params;
+  var loading_suggestions = $("#loading-tag-suggestions");
+  fetch(api_url)
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((suggestion) => {
+        // create tag suggestion using pill
+        var pill = document.createElement("div");
+        pill.className = "label";
+        pill.onclick = function () {
+          var currentTags = $("#field-tags").val();
+          currentTags = currentTags.split(",");
+          currentTags.push(suggestion);
+          $("#field-tags").val(currentTags.join(","));
+          $("#field-tags").trigger("change.select2");
+        };
+        var plus = document.createElement("i");
+        plus.className = "fa fa-plus";
+        pill.appendChild(plus);
+        var text = document.createElement("span");
+        text.textContent = suggestion;
+        pill.appendChild(text);
+        loading_suggestions.remove();
+        tag_suggestions.append(pill);
+      });
+    });
+}
+/* End of Agrovoc tags */
+
+/* only for /dataset/edit/xxx or /dataset/new */
 if (
   window.location.pathname.includes("/dataset/edit/") ||
   window.location.pathname === "/dataset/new"
 ) {
-  document.addEventListener("DOMContentLoaded", function () {
-    const countryDropdown = document.getElementById("field-country");
+  $(document).ready(function () {
+    var countryDropdown = $("#field-country");
 
-    // Fetch countries from the API when the page loads
+    /* Country list */
     fetch("/api/2/util/countries")
       .then((response) => response.json())
       .then((data) => {
         // Populate the dropdown with countries
         data.forEach((country) => {
-          const option = document.createElement("option");
+          var option = document.createElement("option");
           option.value = country.code;
           option.textContent = country.name;
-          countryDropdown.appendChild(option);
+          countryDropdown.append(option);
         });
       });
-  });
-  $(document).ready(function () {
-    // Select the Select2 element by its ID
+    /* End of Country list */
+
+    /* Agrovoc tags */
     $("#field-tags").on("change.select2", function () {
-      // Get the current value(s)
       const selectedTags = $(this).val();
-      console.log("Selected tags:", selectedTags);
-
-      // Perform your custom action here
-      handleTagChange(selectedTags);
+      var tag_suggestions = $("#tag-suggestions");
+      tag_suggestions.empty();
+      var loading_suggestions = document.createElement("div");
+      loading_suggestions.id = "loading-tag-suggestions";
+      loading_suggestions.className = "text-muted";
+      loading_suggestions.textContent = "Loading suggestions...";
+      tag_suggestions.append(loading_suggestions);
+      handleTagChange(selectedTags.split(","));
+      handleTagChange(selectedTags.split(","), "&category=children");
     });
-
-    // Example custom action
-    function handleTagChange(tags) {
-      alert("Tags updated to: " + tags);
-    }
+    /* End of Agrovoc tags */
   });
 }
--- src/ckanext-agra-theme/ckanext/agra_theme/routes/api/agrovoc.py
+++ src/ckanext-agra-theme/ckanext/agra_theme/routes/api/agrovoc.py
@@ -1,5 +1,5 @@
 import requests
-from flask import jsonify, request
+from flask import jsonify, request, abort
 
 # Constants
 agrovoc_params = {
@@ -13,41 +13,51 @@ agrovoc_params = {
 agrovoc_query = "&".join(
     [f"{key}={value}" for key, value in agrovoc_params.items()]
 )
-min_query_length = 1
+min_query_length = 3
 agrovoc_url = "https://agrovoc.fao.org"
 agrovoc_search_url = f"{agrovoc_url}/browse/rest/v1/search"
-agrovoc_child_url = f"{agrovoc_url}/browse/rest/v1/agrovoc/children?uri="
+agrovoc_suggestion_url = f"{agrovoc_url}/browse/rest/v1/agrovoc/"
 
 
 def api_agrovoc(blueprint):
-    @blueprint.route("/api/2/util/tag/autocomplete", methods=["GET"])
+    @blueprint.route("/api/2/util/tag/suggestion", methods=["GET"])
     def autocomplete():
-        # Get the 'incomplete' query parameter from the request
-        incomplete = request.args.get("incomplete", "")
-        if not incomplete:
-            return jsonify({"ResultSet": {"Result": []}})
-        if len(incomplete) < min_query_length:
-            return jsonify({"ResultSet": {"Result": []}})
+        # Get the 'q' query parameter from the request
+        q = request.args.get("q", "")
+        category = request.args.get("category", "broader")
+        if not q:
+            return abort(404)
+        if len(q) < min_query_length:
+            return abort(404)
 
         # Call the AGROVOC API
         # https://www.fao.org/agrovoc/machine-use
-        search_url = f"{agrovoc_search_url}?query={incomplete}*&{agrovoc_query}"
-        response = requests.get(search_url)
+        search_url = f"{agrovoc_search_url}?query={q}*&{agrovoc_query}"
+        res = requests.get(search_url)
 
         # Check if the AGROVOC API call was successful
-        if response.status_code != 200:
-            return jsonify({"ResultSet": {"Result": []}})
+        if res.status_code != 200:
+            return abort(404)
+        agrovoc_data = res.json()
+        content_url = agrovoc_data.get("results")
+        if content_url:
+            content_url = content_url[0].get("uri", "")
+        else:
+            return abort(404)
+        res = requests.get(
+            f"{agrovoc_suggestion_url}{category}?uri={content_url}"
+        )
+        agrovoc_suggestion_data = res.json()
 
-        agrovoc_data = response.json()
-        content_url = agrovoc_data.get("results")[0].get("uri", "")
-        response = requests.get(f"{agrovoc_child_url}{content_url}")
-        agrovoc_child_data = response.json()
+        data_key = "broader"
+        if category == "children":
+            data_key = "narrower"
 
         # Transform the AGROVOC API response to CKAN's expected format
         results = [
-            {"Name": result.get("prefLabel", "")}
-            for result in agrovoc_child_data.get("narrower", [])
+            result.get("prefLabel", "")
+            for result in agrovoc_suggestion_data.get(data_key, [])
             if "prefLabel" in result
         ]
 
-        return jsonify({"ResultSet": {"Result": results}})
+        return jsonify(results)
