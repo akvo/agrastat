d2b0ddc6e8b1d3acbe8f8edeea1d62964b1d0714
dedenbangkit <mail@dedenbangkit.com>
2025-01-29 18:14:04 +0700

    [#14] Schema Draft

--- src/ckanext-agra-theme/ckanext/agra_theme/plugin.py
+++ src/ckanext-agra-theme/ckanext/agra_theme/plugin.py
@@ -1,8 +1,5 @@
-import os
-import gettext
 from flask import Blueprint, render_template, request, abort, jsonify
 import ckan.plugins as plugins
-import logging
 import ckan.plugins.toolkit as toolkit
 from .routes.api.agrovoc import api_agrovoc
 from .routes.api.countries import api_countries
@@ -16,6 +13,15 @@ api_agrovoc(agra_blueprint)
 api_countries(agra_blueprint)
 page_statistic(agra_blueprint)
 
+extra_fields = [
+    "business_line",
+    "source",
+    "country",
+    "methodology",
+    "pii",
+    "anon",
+]
+
 
 class AgraThemePlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
     plugins.implements(plugins.IConfigurer)
@@ -23,7 +29,7 @@ class AgraThemePlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
     plugins.implements(plugins.IBlueprint)
     plugins.implements(plugins.IRoutes, inherit=True)
     plugins.implements(plugins.ITemplateHelpers)
-    # plugins.implements(plugins.IDatasetForm)
+    plugins.implements(plugins.IDatasetForm, inherit=False)
 
     def get_helpers(self):
         return {
@@ -62,3 +68,39 @@ class AgraThemePlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
         toolkit.add_template_directory(config_, "templates")
         toolkit.add_public_directory(config_, "public")
         toolkit.add_resource("fanstatic", "agra_theme")
+
+    # IDatasetForm
+    def is_fallback(self):
+        """Indicates that this is not the fallback dataset schema."""
+        return True
+
+    def package_types(self):
+        """Specify the package types that this form should handle."""
+        return ["dataset"]
+
+    def _modify_schema(self, schema):
+        for new_field in extra_fields:
+            schema.update(
+                {
+                    new_field: [
+                        toolkit.get_validator("ignore_missing"),
+                        toolkit.get_converter("convert_to_extras"),
+                    ]
+                }
+            )
+        return schema
+
+    def create_package_schema(self):
+        """Return the schema for package creation."""
+        schema = super(AgraThemePlugin, self).create_package_schema()
+        return self._modify_schema(schema)
+
+    def update_package_schema(self):
+        """Return the schema for package updates."""
+        schema = super(AgraThemePlugin, self).update_package_schema()
+        return self._modify_schema(schema)
+
+    def show_package_schema(self):
+        """Return the schema for package display."""
+        schema = super(AgraThemePlugin, self).show_package_schema()
+        return self._modify_schema(schema)
