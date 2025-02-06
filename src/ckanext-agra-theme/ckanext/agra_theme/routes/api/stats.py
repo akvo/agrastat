452561df2da1d9fb4b7b18d62b97115b36736560
dedenbangkit <mail@dedenbangkit.com>
2025-02-06 13:35:15 +0700

    [#23] Dataset Sort, add limit to API

--- src/ckanext-agra-theme/ckanext/agra_theme/routes/api/stats.py
+++ src/ckanext-agra-theme/ckanext/agra_theme/routes/api/stats.py
@@ -8,35 +8,32 @@ def api_stats(blueprint):
     @blueprint.route("/api/2/statistic/<vocab>", methods=["GET"])
     def statistic(vocab):
         vocab = vocab.lower()
+        limit = request.args.get("limit", "")
         vocab_obj = Vocabulary.get(vocab)
         if not vocab_obj:
             return jsonify({"error": "Vocabulary not found"}), 404
-
-        # All Counts
-        # counts = (
-        #     Session.query(
-        #         Tag.name, func.count(PackageTag.tag_id)
-        #     )  # Use the imported func
-        #     .join(PackageTag, Tag.id == PackageTag.tag_id)
-        #     .filter(Tag.vocabulary_id == vocab_obj.id)
-        #     .group_by(Tag.name)
-        #     .all()
-        # )
-
-        # Top 5
-        counts = (
-            Session.query(Tag.name, func.count(PackageTag.tag_id))
-            .join(PackageTag, Tag.id == PackageTag.tag_id)
-            .filter(Tag.vocabulary_id == vocab_obj.id)
-            .group_by(Tag.name)
-            .order_by(
-                func.count(PackageTag.tag_id).desc()
-            )  # Order by count DESCENDING
-            .limit(5)  # Limit to the top 5
-            .all()
-        )
-
+        if limit:
+            counts = (
+                Session.query(Tag.name, func.count(PackageTag.tag_id))
+                .join(PackageTag, Tag.id == PackageTag.tag_id)
+                .filter(Tag.vocabulary_id == vocab_obj.id)
+                .group_by(Tag.name)
+                .order_by(
+                    func.count(PackageTag.tag_id).desc()
+                )  # Order by count DESCENDING
+                .limit(limit)  # Limit to the top 5
+                .all()
+            )
+        else:
+            counts = (
+                Session.query(
+                    Tag.name, func.count(PackageTag.tag_id)
+                )  # Use the imported func
+                .join(PackageTag, Tag.id == PackageTag.tag_id)
+                .filter(Tag.vocabulary_id == vocab_obj.id)
+                .group_by(Tag.name)
+                .all()
+            )
         # Format the results into a list of dictionaries
         results = [{"name": name, "value": count} for name, count in counts]
-
         return jsonify(results)
