ac9814732e791bc7f8e431fe59e2911f33d6e6f4
dedenbangkit <mail@dedenbangkit.com>
2025-02-13 15:24:51 +0700

    [#28] Add Documentation

---
+++ update-documentation.sh
@@ -0,0 +1,4 @@
+docker run -it --rm -v "$(pwd)/docs:/docs" \
+    akvo/akvo-sphinx:20220525.082728.594558b make html
+
+cp -r ./docs/build/html ./src/ckanext-agra-theme/ckanext/agra_theme/public/documentation
