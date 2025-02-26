6ab5a55956432f6ecc4655c4745f8d2256c8a12a
Anjar Fiandrianto <anjar@akvo.org>
2025-02-26 16:46:59 +0700

    Modify script to generate the documentation

--- update-documentation.sh
+++ update-documentation.sh
@@ -1,4 +1,7 @@
-docker run -it --rm -v "$(pwd)/docs:/docs" \
+#!/usr/bin/sh
+set -eu
+
+docker run -i --rm -v "$(pwd)/docs:/docs" \
     akvo/akvo-sphinx:20220525.082728.594558b make html
 
-cp -r ./docs/build/html ./src/ckanext-agra-theme/ckanext/agra_theme/public/documentation
+cp -r ./docs/build/html/* ./src/ckanext-agra-theme/ckanext/agra_theme/public/documentation
