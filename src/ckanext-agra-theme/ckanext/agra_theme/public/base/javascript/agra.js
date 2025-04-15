f4d4f2b60d0352f6ee7402bb8ec8946ec5f6c9e1
dedenbangkit <mail@dedenbangkit.com>
2025-04-15 12:47:05 +0700

    [#49] Homepage update

--- src/ckanext-agra-theme/ckanext/agra_theme/public/base/javascript/agra.js
+++ src/ckanext-agra-theme/ckanext/agra_theme/public/base/javascript/agra.js
@@ -112,7 +112,7 @@ function createRadarOption(data) {
 function renderChart(orgName = null) {
   const header = document.querySelector(".masthead");
   const navLinks = document.querySelectorAll(".masthead .nav > li > a");
-  header.style.backgroundColor = "#08743f";
+  header.style.backgroundColor = "rgba(114, 191, 68, 0.7)";
   navLinks.forEach((link) => {
     link.style.color = "white";
   });
